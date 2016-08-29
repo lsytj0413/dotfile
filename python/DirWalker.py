0d8c198f3891650b37ad9db18b8e39a918504742
lsytj0413 <511121939@qq.com>
2016-08-29 11:16:42 +0800

    修改为装饰器实现

--- python/DirWalker.py
+++ python/DirWalker.py
@@ -10,8 +10,28 @@ class WalkType(Enum):
 
 
 def walk(type):
+    '''
+    遍历目录
+
+    args:
+        type: 遍历类型, WalkType
+    '''
+    assert isinstance(type, (WalkType))
+
     def walk_imp(fn):
+        '''
+        处理函数
+
+        args:
+            fn: 包裹函数
+        '''
         def walk_wrapper(dir_path):
+            '''
+            遍历目录
+
+            args:
+                dir_path: 目录路径
+            '''
             import os
             import sys
             for file_name in os.listdir(dir_path):
@@ -29,4 +49,13 @@ def walk(type):
 
 
 if __name__ == '__main__':
+    @walk(WalkType.FILE)
+    def printer(file_name):
+        print file_name
+        import os
+        if os.path.isdir(file_name):
+            print "Dir"
+            printer(file_name)
+
+    printer("/home/lsytj/dotfile")
     print 'DirWalker module'

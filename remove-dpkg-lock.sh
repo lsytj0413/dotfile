f5ffdb9f86227192b861bc57bd34d0355265ee1e
lsytj0413 <511121939@qq.com>
2017-05-20 22:04:42 +0800

    root permissions check

--- remove-dpkg-lock.sh
+++ remove-dpkg-lock.sh
@@ -1,6 +1,12 @@
 # /bin/bash
 
-# Need sudo
+# 本脚本需要使用管理员权限启动
+if [ $(id -u) -ne 0 ]; then
+    echo "this script must be run at *ROOT* permissions." >&2
+    exit 1
+fi
 
 rm /var/cache/apt/archives/lock
-rm /var/lib/dpkg/lock 
+rm /var/lib/dpkg/lock
+
+exit 0
--- ubuntu16-init.sh
+++ ubuntu16-init.sh
@@ -1,15 +1,20 @@
 # /bin/bash
+
 # 本脚本需要使用管理员权限启动
+if [ $(id -u) -ne 0 ]; then
+    echo "this script must be run at *ROOT* permissions." >&2
+    exit 1
+fi
 
 # 1. 安装媒体解码器
-apt install ubuntu-restricted-extras
+apt install -y ubuntu-restricted-extras
 
 # 2. 移动启动栏位置
 gsettings set com.canonical.Unity.Launcher launcher-position Botton
 
 # 3. 删除Amazon
-apt remove unity-lens-shopping
-apt remove unity-webapps-common
+apt remove -y unity-lens-shopping
+apt remove -y unity-webapps-common
 
 # 4. 升级系统补丁
 apt update -y
@@ -18,59 +23,64 @@ apt autoclean -y
 apt clean -y
 
 # 5. 删除libreoffice
-apt remove libreoffice-common
+apt remove -y libreoffice-common
 
 # 6. 删除多余的自带软件
-apt remove thunderbird totem rhythmbox empathy
-apt remove brasero simple-scan gnome-mahjongg
-apt remove aisleriot gnome-mines cheese
-apt remove gransmission-common gnome-orca
-apt remove webbrowser-app gnome-sudoku landscape-client-ui-install
-apt remove onboard deja-dup
+apt remove -y thunderbird totem rhythmbox empathy
+apt remove -y brasero simple-scan gnome-mahjongg
+apt remove -y aisleriot gnome-mines cheese
+apt remove -y gransmission-common gnome-orca
+apt remove -y webbrowser-app gnome-sudoku landscape-client-ui-install
+apt remove -y onboard deja-dup
 
 # 7. 安装vim
-apt install vim
+apt install -y vim
 
 # 8. 安装chrome
-apt install chrominm-browser
-apt install libappindicator1 libindicator7
+apt install -y chrominm-browser
+apt install -y libappindicator1 libindicator7
 apt -f install
 
 # 9. 安装搜狗输入法
-apt install sogoupinyin
+apt install -y sogoupinyin
 
 # 10. 安装wps
-apt install wap-office
+apt install -y wap-office
 
 # 11. 安装cmake, git
-apt install cmake
-apt install git
+apt install -y cmake
+apt install -y git
 
 # 12. 安装unrar
-apt install unrar
+apt install -y unrar
 
 # 13. 安装java8
 # 添加PPA源
-app-apt-repository ppa:webupd8team/java
+apt-get install -y software-properties-common
+app-apt-repository -y ppa:webupd8team/java
 # 更新源
 apt-get update
 # 安装oracle java8
-apt install oracle-java8-installer
+apt install -y oracle-java8-installer
 # 查看java版本
 java -version
 # 设置java8环境变量
-apt install oracle-java8-set-default
+apt install -y oracle-java8-set-default
 # java版本切换
 update-java-alternatives -s java-8-oracle
 
-
 # 14. 安装alien, 一个rpm包转换为deb包的工具
-apt install alien
+apt install -y alien
 
 # 15. 安装cloc, 代码行统计工具
-apt install cloc
+apt install -y cloc
 
 # golang1.8
-sudo add-apt-repository ppa:longsleep/golang-backports 
-sudo apt-get update 
-sudo apt-get install golang-go
+add-apt-repository -y ppa:longsleep/golang-backports
+apt-get update
+apt-get install -y golang-go
+
+# emacs
+apt install emacs
+
+exit 0

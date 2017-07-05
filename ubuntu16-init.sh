fb4ff83bebca123f9114f2623cc72e0cd27396aa
lsytj0413 <511121939@qq.com>
2017-07-05 13:39:22 +0800

    vscode

--- ubuntu16-init.sh
+++ ubuntu16-init.sh
@@ -83,4 +83,11 @@ apt-get install -y golang-go
 # emacs
 apt install emacs
 
+# vscode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt update
+apt install code
+
 exit 0

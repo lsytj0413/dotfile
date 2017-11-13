97668e8aa20d25c08315bd110ae8737246d62e66
lsytj0413 <511121939@qq.com>
2017-11-13 21:42:16 +0800

    update

--- ubuntu16-init.sh
+++ ubuntu16-init.sh
@@ -6,6 +6,8 @@ if [ $(id -u) -ne 0 ]; then
     exit 1
 fi
 
+echo "password" | sudo -S
+
 remove_unused_software() {
     echo "-> Start remove unused software ..."
 
@@ -24,6 +26,8 @@ remove_unused_software() {
     apt remove -y webbrowser-app gnome-sudoku landscape-client-ui-install
     apt remove -y onboard deja-dup
 }
+
+remove_unused_software
 
 # 1. 安装媒体解码器
 apt install -y ubuntu-restricted-extras
@@ -61,61 +65,71 @@ apt install -y git
 # 12. 安装unrar
 apt install -y unrar
 
-# 13. 安装java8
-# license
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-# 添加PPA源
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:webupd8team/java
-# 更新源
-apt-get update
-# 安装oracle java8
-apt install -y oracle-java8-installer
-# 查看java版本
-java -version
-# 设置java8环境变量
-apt install -y oracle-java8-set-default
-# java版本切换
-update-java-alternatives -s java-8-oracle
+# 安装 Java8
+install_oracle_java8() {
+    # license
+    echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+    # 添加PPA源
+    apt-get install -y software-properties-common
+    add-apt-repository -y ppa:webupd8team/java
+    # 更新源
+    apt-get update
+    # 安装oracle java8
+    apt install -y oracle-java8-installer
+    # 查看java版本
+    java -version
+    # 设置java8环境变量
+    apt install -y oracle-java8-set-default
+    # java版本切换
+    update-java-alternatives -s java-8-oracle
+}
+
+# 安装 golang1.8
+install_golang1.8() {
+    # golang1.8
+    add-apt-repository -y ppa:longsleep/golang-backports
+    apt-get update
+    apt-get install -y golang-go
+    apt-get install -y golang-golang-x-tools
+}
+
+# 安装 Emacs25
+install_emacs25() {
+    sudo add-apt-repository -y ppa:kelleyk/emacs
+    sudo apt update
+    sudo apt install -y emacs25
+}
+
+# 安装 vscode
+install_vscode() {
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    apt update
+    apt install code
+}
+
+# 安装 docker-ce
+install_docker() {
+    apt install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    apt-key fingerprint 0EBFCD88
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+    apt update
+    # 安装 latest 版本的 docker-ce
+    apt install docker-ce
+    # 获取 docker-ce 版本
+    apt-cache madison docker-ce
+    # 安装特定版本的 docker-ce
+    # apt install docker-ce=<version>
+}
 
 # 14. 安装alien, 一个rpm包转换为deb包的工具
 # apt install -y alien
 
 # 15. 安装cloc, 代码行统计工具
 apt install -y cloc
-
-# golang1.8
-add-apt-repository -y ppa:longsleep/golang-backports
-apt-get update
-apt-get install -y golang-go
-apt-get install -y golang-golang-x-tools
-
-# emacs25
-sudo add-apt-repository -y ppa:kelleyk/emacs
-sudo apt update
-sudo apt install -y emacs25
-
-# vscode
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-apt update
-apt install code
-
-# docker
-
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-sudo apt update
-# 安装 latest 版本的 docker-ce
-sudo proxychains4 apt install docker-ce
-# 获取 docker-ce 版本
-sudo apt-cache madison docker-ce
-# 安装特定版本的 docker-ce
-# apt install docker-ce=<version>
 
 exit 0

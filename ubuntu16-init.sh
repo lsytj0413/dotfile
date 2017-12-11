faccccc92e541c318a278abbf6a29c16fba50e86
lsytj0413 <511121939@qq.com>
2017-12-11 15:30:53 +0800

    update install_mongodb

--- ubuntu16-init.sh
+++ ubuntu16-init.sh
@@ -136,6 +136,7 @@ install_nodejs8() {
 install_mongodb() {
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
     echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+    apt install mongodb-org
 }
 
 # 14. 安装alien, 一个rpm包转换为deb包的工具
